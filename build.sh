0bb6e03831a97c56273636cdd322924402bcf5a0
Jamong <arozic425@gmail.com>
2024-11-24 19:46:30 +0900

    ✨ tailwind-cn 함수 추가 및 자동 배포 github action 파일 설정 (#3)
    * 🎉 Init: storybook 세팅 및 모바일 레이아웃 규격 설정
    
    * ✨ Feat: tailwind-cn 함수 추가
    
    * 🔨 Chore: github-action용 파일 설정

---
+++ build.sh
@@ -0,0 +1,5 @@
+#automatic deployment option
+cd ../
+mkdir output
+cp -R ./Hobiday-frontend/* ./output
+cp -R ./output ./Hobiday-frontend/
---
+++ src/components/button/button.tsx
@@ -0,0 +1,8 @@
+import { cva } from "class-variance-authority";
+
+export default function button() {
+  const buttonVariants = cva(``, {
+    variants: {},
+  });
+  return <div></div>;
+}
