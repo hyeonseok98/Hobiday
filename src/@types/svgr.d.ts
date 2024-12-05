2be2725598a6408c6cff2d7ce2a17f6c95afbb07
LGY <leegyoung12@gmail.com>
2024-12-05 14:08:04 +0900

    🚚 Rename: '@types'폴더 root로 이동

--- src/@types/svgr.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC, SVGProps, SVGSVGElement } from "react";
-
-declare global {
-  module "*.svg" {
-    const component: FC<SVGProps<SVGSVGElement>>;
-
-    export default component;
-  }
-}
