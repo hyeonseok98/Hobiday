Hobiday/Hobiday-frontend

✨ Feat: Kakao SDK Script 추가

src/app/layout.tsx
@@ -2,6 +2,7 @@ import ReactQueryProvider from "@/contexts/providers/query.provider";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import KakaoScript from "@/components/_providers/kakao-script";
 
 const pretendard = localFont({
   src: "../styles/fonts/PretendardVariable.woff2",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={`${pretendard.className} antialiased max-w-[430px] w-full mx-auto`}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
+      <KakaoScript />
     </html>
   );
 }

hyeonseok98/Hobiday

src/app/layout.tsx
@@ -1,8 +1,8 @@
+import KakaoScript from "@/contexts/providers/kakao-script";
 import ReactQueryProvider from "@/contexts/providers/query.provider";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import KakaoScript from "@/contexts/providers/kakao-script";
 
 const pretendard = localFont({
   src: "../styles/fonts/PretendardVariable.woff2",